542ab8e5b5cba9e4977a6ae00272afbabf54a494
alexcl11 <aleecanovaslopez@gmail.com>
2026-04-16 09:18:14 +0200

    up

---
+++ MvcCoreSaSAzureStorage/Services/ServiceAzureAlumnos.cs
@@ -0,0 +1,14 @@
+﻿using Azure.Data.Tables;
+
+namespace MvcCoreSaSAzureStorage.Services
+{
+    public class ServiceAzureAlumnos
+    {
+        private TableClient tableAlumnos;
+        private string urlApi;
+        public ServiceAzureAlumnos(IConfiguration configuration)
+        {
+            this.urlApi = configuration.GetValue<string>("ApiUrls:ApiAzureToken");
+        }
+    }
+}
